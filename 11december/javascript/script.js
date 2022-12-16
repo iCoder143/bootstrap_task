8449fd03b67df9175aed9233df4f041f740a205b
iCoder143 <maliktanzeel1025@gmail.com>
2022-12-16 16:46:12 +0500

    my first task of git

---
+++ 11december/javascript/script.js
@@ -0,0 +1,17 @@
+// var name ="Ali"
+// console.log(name)
+
+// let x=5;
+// let y=6;
+
+// let result =x*y;
+// document.write(`<br> Multiplying ${x} and ${y} is ${result}`)
+let name ="Tanzeel Ur Rehman";
+let age = 25;
+let degree ="BSCS"
+let plan ="Become MERN Stack Developer";
+
+
+
+
+console.log(`Hi my name is ${name} and age is ${age} i have done ${degree} i wana ${plan}`)
